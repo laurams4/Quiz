7bb0a51814bed1d37ea946bad5bc8a6642f56df3
laurams4 <lauramsotodosos@gmail.com>
2025-02-04 21:18:04 +0100

    Variable score a 0 en vez de a null

--- ranking.js
+++ ranking.js
@@ -1,8 +1,9 @@
 const textScore = document.getElementById("textScore");
 
-const scoreActual = localStorage.getItem("score");
+const scoreActual = localStorage.getItem("score") ?? 0;
 textScore.innerText = `¡Has conseguido ${scoreActual} puntos! 🎉`;
 
 function restartQuiz() {
+    localStorage.setItem("score", 0); // Reinicia la puntuación
     window.location = "/quiz.html";
 }
